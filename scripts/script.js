806dc0a0f45ae793d64881c5599a2e43da65ae52
Muflih Abqori <muflihabqori@gmail.com>
2025-08-04 21:31:57 +0700

    Minor changes on responsive

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#menu-icon');
-const navList = document.querySelector('.nav-list');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navList.classList.toggle('open');
-}
